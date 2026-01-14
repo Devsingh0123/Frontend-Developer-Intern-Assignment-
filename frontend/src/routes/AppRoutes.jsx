87d03e98655d2cfd3bfa50b4eb8b68820a7a0b4f
Devsingh0123 <dayamns20@gmail.com>
2026-01-15 01:27:33 +0530

    fix error while rendering

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Dashboard from "../pages/Dashboard";
+import Dashboard from "../pages/DashBoard";
 import Profile from "../pages/Profile";
 import PageNotFound from "../pages/PageNotFound"
 import ProtectedRoute from "./ProtectedRoute";
