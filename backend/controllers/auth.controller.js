06ffb0e53e658875d7dcee5e25696ffdc5352971
Devsingh0123 <dayamns20@gmail.com>
2026-01-15 12:36:19 +0530

    cookie check

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,10 +54,8 @@ export const loginUser = async (req, res) => {
   );
 
   res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,        
-  sameSite: "none", 
-  maxAge: 24 * 60 * 60 * 1000,}).json({user});
+  
+  maxAge: 7 * 24 * 60 * 60 * 1000,}).json({user});
 };
 
 
